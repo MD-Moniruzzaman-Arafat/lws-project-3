ecf20446b65dda987530ee8369fd7ec3a752dcc9
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-12 12:02:44 +0600

    headerNavigation component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import AnnouncementBar from './components/AnnouncementBar/AnnouncementBar';
+import HeaderNavigation from './components/HeaderNavigation/HeaderNavigation';
 
 function App() {
   return (
     <>
       <AnnouncementBar />
+      <HeaderNavigation />
     </>
   );
 }
---
+++ src/components/HeaderNavigation/Menu.jsx
@@ -0,0 +1,20 @@
+export default function Menu() {
+  return (
+    <>
+      <nav className="hidden md:flex space-x-6">
+        <a href="#" className="hover:text-gray-500 transition-colors">
+          Shop
+        </a>
+        <a href="#" className="hover:text-gray-500 transition-colors">
+          On Sale
+        </a>
+        <a href="#" className="hover:text-gray-500 transition-colors">
+          New Arrivals
+        </a>
+        <a href="#" className="hover:text-gray-500 transition-colors">
+          Brands
+        </a>
+      </nav>
+    </>
+  );
+}
