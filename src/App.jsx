aa074024087af55f5b3675ea35702338ec19e245
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-12 11:35:01 +0600

    announcementBar component design

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import AnnouncementBar from './components/AnnouncementBar/AnnouncementBar';
+
+function App() {
+  return (
+    <>
+      <AnnouncementBar />
+    </>
+  );
+}
+
+export default App;
