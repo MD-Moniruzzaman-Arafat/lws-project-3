49734433be50029860feb4a0e801ad16fe1f3786
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-12 18:19:34 +0600

    show fake data using json data

---
+++ src/components/Products/ProductCard.jsx
@@ -0,0 +1,48 @@
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
+
+export default function ProductCard({ product }) {
+  return (
+    <>
+      <div className="bg-gray-100 rounded-lg overflow-hidden transition-transform hover:scale-[1.02] duration-300">
+        <div className="h-48 bg-gray-200 flex items-center justify-center">
+          <img
+            src={product.image}
+            alt="Polo with Tipping Details"
+            className="h-full w-auto object-cover"
+          />
+        </div>
+        <div className="p-4">
+          <h3 className="font-medium">{product.name}</h3>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center my-1">
+              <Rating
+                style={{ maxWidth: 70 }}
+                value={product.rating}
+                readOnly
+              />
+              <span className="text-xs text-gray-500 ml-1">
+                {product.rating}/5
+              </span>
+            </div>
+            <span className="text-xs text-gray-700">
+              ({product.stock} pcs left)
+            </span>
+          </div>
+          <div className="flex items-center">
+            <p className="font-bold">${product.price}</p>
+            {product?.oldPrice && (
+              <p className="text-gray-400 line-through ml-2">
+                ${product?.oldPrice}
+              </p>
+            )}
+          </div>
+
+          <button className="disabled:bg-gray-700 disabled:text-gray-400 disabled:cursor-not-allowed w-full mt-2 bg-gray-800 py-1 text-gray-100 rounded flex items-center justify-center active:translate-y-1 transition-all active:bg-gray-900">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
