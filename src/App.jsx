4849fa3c2c981fd0a60e41555f4f6d2e33be92d1
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-12 16:10:35 +0600

    product and cart component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
 import AnnouncementBar from './components/AnnouncementBar/AnnouncementBar';
+import Cart from './components/Cart/Cart';
 import HeaderNavigation from './components/HeaderNavigation/HeaderNavigation';
+import Products from './components/Products/Products';
 
 function App() {
   return (
     <>
       <AnnouncementBar />
       <HeaderNavigation />
+      <main className="container mx-auto px-4 md:px-8 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <Products />
+          <Cart />
+        </div>
+      </main>
     </>
   );
 }
